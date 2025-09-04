39e12b864416d73c127d268e80aa3fd6bf240a24
Suvasree sarkar <suvasree.sarkar@stu.adamasuniversity.ac.in>
2025-09-04 20:25:03 +0530

    Uncomment example tests for climbStairs function

---
+++ four.py
@@ -0,0 +1,9 @@
+def climbStairs(n: int) -> int:
+    if n <= 2:
+        return n
+    a, b = 1, 2
+    for _ in range(3, n+1):
+        a, b = b, a + b
+    return b
+print(climbStairs(2))  
+print(climbStairs(3))  
